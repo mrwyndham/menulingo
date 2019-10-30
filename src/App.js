48512684846b88f99914899861dfbb14ad9a3298
Sam <samwyndhamii@gmail.com>
2019-10-30 12:31:00 +0800

    added routing

--- src/App.js
+++ src/App.js
@@ -1,67 +1,16 @@
 import React from 'react';
-import './App.scss';
-import logo from './components/layout/logo/icon_primary.svg';
-import english from './components/widgets/LanguagePicker/english.svg';
-import bg from './assets/bg.jpg';
-
-let primaryColor = '#E02424';
-let fontFamily = 'roboto, sans-serif';
-
-let style = {
-  featureBackground: {
-    backgroundColor: primaryColor
-  },
-  contentBackground: {
-    backgroundImage: `url()`,
-    backgroundPosition: '25% 70%',
-    backgroundSize: 'auto 120vh, cover'
-  },
-  underlineColor: {
-    border: `solid ${primaryColor} 0.25rem`
-  },
-  titleText: {
-    fontFamily: fontFamily,
-    color: primaryColor,
-    fontWeight: 'bold',
-    fontSize: '1.75rem'
-  },
-  menuButtonColor: {
-    fontFamily: fontFamily,
-    color: '#ffffff',
-    fontSize: '1rem',
-    backgroundColor: primaryColor
-  }
-};
+import Start from './components/pages/Start';
+import Menu from './components/pages/Menu';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div className="App">
-      <div className="App--Feature" style={style.featureBackground}></div>
-      <div className="App--Content" style={style.contentBackground}>
-        <div>
-          <img className="Language" src={english} alt="" />
-        </div>
-        <div>
-          <div className="Logo">
-            <img src={logo} alt="" />
-          </div>
-          <div className="Title">
-            <h1 className="Title--Heading" style={style.titleText}>
-              PYSCO
-            </h1>
-            <span className="Title--Underline">
-              <hr style={style.underlineColor} />
-              <hr style={style.underlineColor} />
-            </span>
-          </div>
-        </div>
-        <div>
-          <button className="Button--Menu" style={style.menuButtonColor}>
-            MENU
-          </button>
-        </div>
+    <Router>
+      <div>
+        <Route path="/" exact component={Start} />
+        <Route path="/menu" component={Menu} />
       </div>
-    </div>
+    </Router>
   );
 };
 
---
+++ src/components/pages/Start.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import './Start.scss';
+import logo from '../../components/layout/logo/icon_primary.svg';
+import english from '../../components/widgets/LanguagePicker/english.svg';
+import bg from '../../assets/bg.jpg';
+import { Link } from 'react-router-dom';
+
+let primaryColor = '#E02424';
+let fontFamily = 'roboto, sans-serif';
+
+let style = {
+  featureBackground: {
+    backgroundColor: primaryColor
+  },
+  contentBackground: {
+    backgroundImage: `url()`,
+    backgroundPosition: '25% 70%',
+    backgroundSize: 'auto 120vh, cover'
+  },
+  underlineColor: {
+    border: `solid ${primaryColor} 0.25rem`
+  },
+  titleText: {
+    fontFamily: fontFamily,
+    color: primaryColor,
+    fontWeight: 'bold',
+    fontSize: '1.75rem'
+  },
+  menuButtonColor: {
+    fontFamily: fontFamily,
+    color: '#ffffff',
+    fontSize: '1rem',
+    backgroundColor: primaryColor
+  }
+};
+
+const Start = () => {
+  return (
+    <div className="App">
+      <div className="App--Feature" style={style.featureBackground}></div>
+      <div className="App--Content" style={style.contentBackground}>
+        <div>
+          <img className="Language" src={english} alt="" />
+        </div>
+        <div>
+          <div className="Logo">
+            <img src={logo} alt="" />
+          </div>
+          <div className="Title">
+            <h1 className="Title--Heading" style={style.titleText}>
+              PYSCO
+            </h1>
+            <span className="Title--Underline">
+              <hr style={style.underlineColor} />
+              <hr style={style.underlineColor} />
+            </span>
+          </div>
+        </div>
+        <div>
+          <Link to="/menu">
+            <button className="Button--Menu" style={style.menuButtonColor}>
+              MENU
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Start;
