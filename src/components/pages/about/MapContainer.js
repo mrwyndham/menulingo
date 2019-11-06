2a0189b4d0ea5de03ca57bf44cc899020a545bfe
Sam <samwyndhamii@gmail.com>
2019-11-06 23:18:48 +0800

    Finished version 1 of the about page with google-maps-react component and the api

---
+++ src/components/pages/about/MapContainer.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { Map, GoogleApiWrapper } from 'google-maps-react';
+
+const mapStyles = {
+  width: '100%',
+  height: '100%',
+  left: '0',
+  top: '0',
+  borderRadius: '10px'
+};
+
+export class MapContainer extends Component {
+  render() {
+    return (
+      <Map
+        google={this.props.google}
+        zoom={14}
+        style={mapStyles}
+        initialCenter={{
+          lat: -1.2884,
+          lng: 36.8233
+        }}
+      />
+    );
+  }
+}
+
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyBMESC4yri5c393xcIft3WbcNWDCcvjXPY'
+})(MapContainer);
