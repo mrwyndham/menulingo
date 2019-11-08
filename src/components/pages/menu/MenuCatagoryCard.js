f0ebdc53cd0635026715fc3b9eed1eb70b453f2c
Sam <samwyndhamii@gmail.com>
2019-11-08 14:36:40 +0800

    CSS changes to reflect spacing, catagories now with background fliter

--- src/components/pages/menu/MenuCatagoryCard.js
+++ src/components/pages/menu/MenuCatagoryCard.js
@@ -6,16 +6,19 @@ const MenuCatagoryCard = props => {
     contentBackground: {
       backgroundImage: `url(${props.image})`,
       backgroundPosition: '25% 70%',
-      backgroundSize: 'auto 120vh, cover'
+      backgroundSize: 'auto 120vh, cover',
+      filter: 'brightness(40%)'
     }
   };
   return (
-    <div>
-      <div className="MenuCatagory" style={style.contentBackground}>
-        <div className="MenuCatagory--Text">
-          <div className="MenuCatagory--Name">{props.name}</div>
-          <div className="MenuCatagory--Description">{props.description}</div>
-        </div>
+    <div className="MenuCatagory">
+      <div
+        className="MenuCatagory--Image"
+        style={style.contentBackground}
+      ></div>
+      <div className="MenuCatagory--Text">
+        <div className="MenuCatagory--Name">{props.name}</div>
+        <div className="MenuCatagory--Description">{props.description}</div>
       </div>
     </div>
   );
