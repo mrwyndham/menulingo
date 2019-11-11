c6de327aeb6f79cc03a936a9d8376579bfaa2aef
Sam <samwyndhamii@gmail.com>
2019-11-11 21:50:53 +0800

    Added stylizing for the add item overlay

--- src/components/pages/menu/MenuItemOrder.js
+++ src/components/pages/menu/MenuItemOrder.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import './MenuItemOrder.scss';
+import Information from './itemOrder/InformationButton';
 
 const MenuItemOrder = props => {
-  return <div onClick={props.onSelectItem}>HelloWorld</div>;
+  return (
+    <div className="MenuItemOrder" onClick={props.onSelectItem}>
+      <Information />
+      <button className="MenuItemOrder--Button">Add Item to Order</button>
+    </div>
+  );
 };
 
 export default MenuItemOrder;
---
+++ src/components/pages/menu/itemOrder/InformationButton.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './InformationButton.scss';
+
+const InformationButton = () => {
+  return (
+    <div className="InformationButton">
+      <span className="InformationButton--Icon">i</span>
+    </div>
+  );
+};
+
+export default InformationButton;
