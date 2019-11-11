46ded2f90c5529b83707a765701ef1e2c051bffd
Sam <samwyndhamii@gmail.com>
2019-11-11 20:47:53 +0800

    Made the menu items stateful

--- src/components/pages/menu/MenuItem.js
+++ src/components/pages/menu/MenuItem.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
+import OrderItem from './MenuItemOrder';
 import './MenuItem.scss';
 
 export class MenuItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      displayOrderItem: false
+    };
+  }
+  handleClick = () => {
+    this.setState(state => ({
+      displayOrderItem: !state.displayOrderItem
+    }));
+  };
   render() {
     return (
-      <div className="MenuItem">
+      <div className="MenuItem" onClick={this.handleClick}>
         <div className="MenuItem--Pic">
           <img src={this.props.item.pic} alt="" />
         </div>
@@ -15,6 +27,7 @@ export class MenuItem extends Component {
           </div>
           <div className="MenuItem--Price">{this.props.item.price} USD</div>
         </div>
+        <div>{this.state.displayOrderItem ? <OrderItem /> : false}</div>
       </div>
     );
   }
---
+++ src/components/pages/menu/MenuItemOrder.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MenuItemOrder = props => {
+  return <div onClick={props.onSelectItem}>HelloWorld</div>;
+};
+
+export default MenuItemOrder;
