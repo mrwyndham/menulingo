b7106570a731421fec6366d5cdb73adb3c7968ca
Sam <samwyndhamii@gmail.com>
2019-11-13 19:14:57 +0800

    Added order popup one at a time

--- src/components/pages/menu/MenuItem.js
+++ src/components/pages/menu/MenuItem.js
@@ -1,45 +1,22 @@
-import React, { Component } from 'react';
-import OrderItem from './MenuItemOrder';
-import './MenuItem.scss';
+import React from "react";
+import "./MenuItem.scss";
 
-export class MenuItem extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      displayOrderItem: false
-    };
-  }
-  handleItemClick = () => {
-    this.setState(state => ({
-      displayOrderItem: !state.displayOrderItem
-    }));
+const MenuItem = props => {
+  const handleItemClick = () => {
+    props.onSelectItem(props.id);
   };
-  handleOrderClick = e => {
-    e.stopPropagation();
-  };
-  render() {
-    return (
-      <div className="MenuItem" onClick={this.handleItemClick}>
-        <div className="MenuItem--Pic">
-          <img src={this.props.item.pic} alt="" />
-        </div>
-        <div className="MenuItem--Text">
-          <div className="MenuItem--Name">{this.props.item.name}</div>
-          <div className="MenuItem--Description">
-            {this.props.item.description}
-          </div>
-          <div className="MenuItem--Price">{this.props.item.price} USD</div>
-        </div>
-        <div>
-          {this.state.displayOrderItem ? (
-            <OrderItem onSelectItem={this.handleOrderClick} />
-          ) : (
-            false
-          )}
-        </div>
+  return (
+    <div className="MenuItem" onClick={handleItemClick}>
+      <div className="MenuItem--Pic">
+        <img src={props.item.pic} alt="" />
       </div>
-    );
-  }
-}
+      <div className="MenuItem--Text">
+        <div className="MenuItem--Name">{props.item.name}</div>
+        <div className="MenuItem--Description">{props.item.description}</div>
+        <div className="MenuItem--Price">{props.item.price} USD</div>
+      </div>
+    </div>
+  );
+};
 
 export default MenuItem;
--- src/components/pages/menu/MenuItemList.js
+++ src/components/pages/menu/MenuItemList.js
@@ -1,23 +1,49 @@
-import React from 'react';
-import MenuItem from './MenuItem';
-import Card from './MenuCatagory';
-import './MenuItemList.scss';
+import React from "react";
+import MenuItem from "./MenuItem";
+import Card from "./MenuCatagory";
+import Order from "./MenuItemOrder";
+import "./MenuItemList.scss";
 
-const MenuItemList = props => {
-  const items = props.location.data.items;
-  const renderItems = Object.keys(items).map(item => (
-    <MenuItem key={items[item].id} item={items[item]} />
-  ));
-  return (
-    <div className="MenuItems">
-      <Card
-        image={props.location.data.pic}
-        name={props.location.data.name}
-        description={props.location.data.description}
-      />
-      {renderItems}
-    </div>
-  );
-};
-
-export default MenuItemList;
+export default class MenuItemList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      reply: null,
+      displayReply: false
+    };
+  }
+  handleSelectItem = id => {
+    this.setState(state => ({
+      reply: id,
+      displayReply: !state.displayReply
+    }));
+  };
+  render() {
+    const items = this.props.location.data.items;
+    const renderItems = Object.keys(items).map(item => (
+      <div className="MenuItems--Content">
+        <MenuItem
+          key={items[item].id}
+          id={items[item].id}
+          item={items[item]}
+          onSelectItem={this.handleSelectItem}
+        />
+        {this.state.displayReply ? (
+          items[item].id === this.state.reply ? (
+            <Order />
+          ) : null
+        ) : null}
+      </div>
+    ));
+    return (
+      <div className="MenuItems">
+        <Card
+          image={this.props.location.data.pic}
+          name={this.props.location.data.name}
+          description={this.props.location.data.description}
+        />
+        {renderItems}
+      </div>
+    );
+  }
+}
