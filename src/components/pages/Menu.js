07e334fa98136aada81eb5eb56f4a256f430727d
Sam Wyndham <samwyndhamii@gmail.com>
2020-02-07 22:25:56 +0800

    Added first api call to backend and enabled CORS on backend

--- src/components/pages/Menu.js
+++ src/components/pages/Menu.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import MenuCatagoryList from "./menu/MenuCatagoryList";
 import "./Menu.scss";
+import axios from "axios";
 
 const Menu = props => {
   const { catagories, order, currency, style } = props;
+  useEffect(() => {
+    axios
+      .get("http://192.168.0.61:5000/api/v1/menus/5e3c0da1c4174b0a6cd0ef52")
+      .then(res => console.log(res.data.data));
+  });
   const renderCatagories = Object.keys(catagories).map(catagory => (
     <MenuCatagoryList
       key={catagories[catagory].id}
